9a1c902daeab51446bb13b5db34ea21c55c83b8d
o_s <huipo22@163.com>
2019-11-05 22:17:01 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vant from 'vant'
 import router from '../router/index'
-import 'vant/lib/index.css'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant)
+
 import axios from 'axios'
 import qs from 'qs';
-
 Vue.config.productionTip = false;
-Vue.prototype.axios = axios; 
-Vue.prototype.qs = qs 
+Vue.prototype.axios = axios;
+Vue.prototype.qs = qs
 //默认后台接口前缀
-axios.defaults.baseURL=''
+axios.defaults.baseURL = ''
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
-Vue.use(Vant);
 
 Vue.config.productionTip = false
 
